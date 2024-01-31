88cecdde3581ac9102615cffa2c2a7168a2a832e
Priyanshu Negi <npriyanshu63@gmail.com>
2024-01-31 20:47:10 +0530

    new problem solved

---
+++ Array and More/initialize-entire-array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[400];
+
+    for(int i=0;i<=400;i++){
+        arr[i] = 1;
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
