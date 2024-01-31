97d9be2b40ca755dddc33b17d2262a36cc25d94e
Priyanshu Negi <npriyanshu63@gmail.com>
2024-01-31 23:37:57 +0530

    modified the binary search func

--- Array and More/BinarySearch.cpp
+++ Array and More/BinarySearch.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-int key,start=0,mid,end=5,found=0;
-int arr[6] ={11,25,32,42,56,62};
-cout<<"Enter the value of key";
-cin>>key;
+int BinarySearch(int arr[],int size,int key){
+    int start=0,mid,end=5;
 
-//   mid = (start + end)/2;
-while (start<=end)
+    while (start<=end)
 {
   mid = (start + end)/2;
 
 if(arr[mid]==key){
-    cout<<"element has found at :"<<mid<<" location";
-break;
+    return mid;
 }
 if(arr[mid]>key){
     end = mid-1;
@@ -22,8 +16,24 @@ if(arr[mid]>key){
 if(arr[mid]<key){
     start = mid +1;
 }
+}
+return 0;
+}
+int main()
+{
+int key;
+int arr[6] ={11,25,32,42,56,62};
+cout<<"Enter the value of key";
+cin>>key;
 
+int loc = BinarySearch(arr,6,key);
+if(loc != 0){
+    cout<<"element has found at :"<<loc<<" location";
+}
+else {
+cout<<"element is not in the array";
 }
     return 0;
 }
+
 
