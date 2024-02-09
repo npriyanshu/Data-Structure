3577f1388c69447a608a18990cb34aff4fdaf676
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-09 17:59:07 +0530

    fix : palindrom with string

--- Character Array And Strings_7/pal.cpp
+++ Character Array And Strings_7/pal.cpp
@@ -1,75 +1,70 @@
-// /*
-// 	Time complexity: O((length(S))
-// 	Space Complexity: O((length(S))
-	
-// 	Where S is the given string. 
-// */
+#include<iostream>
+#include<vector>
+using namespace std;
 
-// void toLowerCase(string &s)
-// {	
-//     // Traverse through the string s
-//     for (int i = 0; i < s.length(); i++)
-//     {
-//         char ch = s[i];
+void toLowerCase(string &s)
+{	
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
         
-//         // Check if ch is a uppercase letter
-//         if (ch <= 'Z' && ch >= 'A')
-//         {
-//             ch = ch - ('A' - 'a');
-//             s[i] = ch;
-//         }
-//     }
-// }
+        if (ch <= 'Z' && ch >= 'A')
+        {
+            ch = ch - ('A' - 'a');
+            s[i] = ch;
+        }
+    }
+}
 
-// // This function reverse the string s
-// string reverseString(string s)
-// {
-//     for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
-//     {
-//         // Swap the ith and jth characters.
-//         char temp = s[i];
-//         s[i] = s[j];
-//         s[j] = temp;
-//     }
+string reverseString(string s)
+{
+    for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
+    {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
     
-//     return s;
-// }
+    return s;
+}
 
-// bool checkPalindrome(string s)
-// {	
-//     // Convert uppercase letter into lowercase letter
-//     toLowerCase(s);
+bool checkPalindrome(string s)
+{	
+    toLowerCase(s);
     
-//     // Find the reverse string of s
-//     string reversedS = reverseString(s);
+    string reversedS = reverseString(s);
 
-//     int n = s.length();
-//     int i = 0;
-//     int j = 0;
+    int n = s.length();
+    int i = 0;
+    int j = 0;
 
-//     while (i < n && j < n)
-//     {
-//         if (isalnum(s[i]) == 0)
-//         {
-//             // ith pointer points to invalid character.
-//             ++i;
-//         }
-//         else if (isalnum(reversedS[j]) == 0)
-//         {
-//             // jth pointer points to invalid character.
-//             ++j;
-//         }
-//         else if (s[i] == reversedS[j])
-//         {
-//             ++i; 
-//             ++j;
-//         }
-//         else
-//         {
-//             return false;
-//         }
-//     }
+    while (i < n && j < n)
+    {
+        if (isalnum(s[i]) == 0)
+        {
+            ++i;
+        }
+        else if (isalnum(reversedS[j]) == 0)
+        {
+            ++j;
+        }
+        else if (s[i] == reversedS[j])
+        {
+            ++i; 
+            ++j;
+        }
+        else
+        {
+            return false;
+        }
+    }
     
-//     return true;
-// }
+    return true;
+}
+int main()
+{
 
+
+
+return 0;
+}
