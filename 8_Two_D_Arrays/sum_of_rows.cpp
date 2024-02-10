b9bdd3e66452b1bd8aeff9a469ea863b6474d1f2
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-10 22:26:58 +0530

    find largest row column

--- 8_Two_D_Arrays/sum_of_rows.cpp
+++ 8_Two_D_Arrays/sum_of_rows.cpp
@@ -2,20 +2,30 @@
 using namespace std;
 int main()
 {
-int arr[3][2]= {{2,4},{1,3},{8,7}};
+int arr[3][2]= {{22,14},{1,33},{8,7}};
 
 // print sum of each rows
+int largeRInd = 0; 
 
+int largestRow =INT_MIN;
 for(int i =0;i<3;i++)
 {
     int sum =0;
     for(int j =0;j<2;j++){
     sum += arr[i][j];
     }
+    if(largestRow<sum){
+    largestRow = sum;
+    largeRInd = i+1;
+}
     cout<<" sum of the "<<i+1<<"st row :"<<sum<<endl;
 }
 
+cout<<"largest row :"<<largeRInd<<endl;
 // print sum of each column
+
+int largestCol = INT_MIN;
+int largeCInd = 0; 
 
 for(int i =0;i<2;i++)
 {
@@ -23,8 +33,13 @@ for(int i =0;i<2;i++)
     for(int j =0;j<3;j++){
     sum += arr[j][i];
     }
+   if(largestRow<sum){
+    largestCol = sum;
+    largeCInd = i+1;
+}
     cout<<" sum of the "<<i+1<<"st column :"<<sum<<endl;
 }
 
+cout<<"largest column :"<<largeCInd<<endl;
 return 0;
 }
