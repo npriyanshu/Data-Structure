d02f9ca451e9214996e1f64b986a75319d019c78
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-16 16:14:00 +0530

    dynamic 2d array

---
+++ 11_static and dynamic memory/dynamic_2D_arr.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // how to make 2d array with heap
+
+    // as we know 
+    // we use pointer to point to memory blocks in heap
+    // eg : int *p = new int[size];
+
+    // we can take each pointer as row here 
+
+    // we use double pointer to point each row in heap
+int n;
+cin>>n;
+// n is the no of rows or addresses
+int **arr = new int*[n];
+
+// now we have an array with n rows or pointers
+// in heap 
+
+// now we have to assign array to each element in arr 
+
+for(int i=0;i<n;i++){
+    arr[i] = new int[n]; // normal syntax 
+}
+
+// creation done
+
+cout<<"enter the value of elemnts"<<endl;
+for(int i = 0; i<n; i++){
+    for(int j = 0;j<n;j++){
+        cin>>arr[i][j];
+    }
+}
+
+cout<<endl;
+// printing the values
+
+for(int i = 0; i<n; i++){
+    for(int j = 0;j<n;j++){
+        cout<<arr[i][j]<<" ";
+    }
+}
+
+
+
+
+return 0;
+}
