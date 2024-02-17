917db893b3074aca6cc83b01a53a023d36f63608
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-17 17:16:31 +0530

    count forward

--- 13_Recursion/count.cpp
+++ 13_Recursion/count.cpp
@@ -7,12 +7,24 @@ void countBack(int n){
     cout<<n<<" ";
     countBack(n-1);
 }
+
+void countFor(int n){
+    if(n == 0) return;
+
+    countFor(n-1);
+
+    cout<<n<<" ";
+}
 int main()
 {
 
 int n;
+cout<<"enter the no :";
 cin>>n;
 countBack(n);
+cout<<endl;
+countFor(n);
+cout<<endl;
 
 return 0;
 }
