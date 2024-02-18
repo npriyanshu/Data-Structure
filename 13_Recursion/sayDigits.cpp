d57cde3ff80a2bd0b45c491e8ceb47689fce43bd
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-18 10:35:33 +0530

    source to destination using recrusion:sayDigits.cpp

---
+++ 13_Recursion/sayDigits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void reachHome(int src,int dest){
+    // base case
+    if(src == dest){
+        cout<<" pahuch gya"<<endl;
+        return;
+    }
+    // processing
+    src++;
+    // recursive call
+    reachHome(src,dest);
+} 
+int main()
+{
+
+int dest = 10;
+int src = 1;
+reachHome(src,dest);
+
+return 0;
+}
---
+++ 13_Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+
+int main()
+{
+
+int dest = 10;
+int src = 1;
