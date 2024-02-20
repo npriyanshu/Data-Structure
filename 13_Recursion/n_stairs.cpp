1626562821450878c3a1669cd35bdc2bea7ee9db
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-20 22:11:41 +0530

    n stairs

--- 13_Recursion/n_stairs.cpp
+++ 13_Recursion/n_stairs.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
+int countDistinctWays(int nStairs) {
+ if(nStairs <0) return 0;
+
+ if(nStairs==0) return 1;
+
+// recursive relation
+return countDistinctWays(nStairs -1 ) + countDistinctWays(nStairs -2 );
+
+}
 int main()
 {
 
+cout<<countDistinctWays(6); // number of ways to climb a staircase with 6 steps.
 
 
 return 0;
