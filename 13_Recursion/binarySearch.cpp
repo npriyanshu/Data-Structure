568ba353477cb24118a2fc6277ceee220a8b8ab6
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-21 21:00:14 +0530

    binary search with recursion

--- 13_Recursion/binarySearch.cpp
+++ 13_Recursion/binarySearch.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
 using namespace std;
-int bSearch(int arr[],int size,int key,int s,int e){
+bool bSearch(int arr[],int key,int s,int e){
     // base case
-    if (size == 0) return -1;
-    
-    int mid = (s + (e-s))/2;
+    if(s>e) return false;
+    int mid = s + (e-s)/2;
+    // if the element is present at the middle
+    if(arr[mid] == key){
+        return  mid;
+    }
+    if(arr[mid]>key){
+        return  bSearch(arr,key,s,mid-1);// recur for left subarray
+    }
+    else{
+        return bSearch(arr,key,mid+1,e);// recur for right subarray
+    }
+   
 }
 int main()
 {
 
-int arr[] ={1,2,3,4,6,8,}; 
-
+int arr[] ={1,2,3,4,6,8,25}; 
+cout<<bSearch(arr,6,0,7);
 return 0;
 }
