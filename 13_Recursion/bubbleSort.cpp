9d47e62d2e720cd3cce5cd956d61cfc494876690
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-24 22:13:42 +0530

    bubble sort

---
+++ 13_Recursion/bubbleSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void sortArray(int *arr,int n){
+
+// base case - already sorted
+if(n == 0 || n == 1){
+    return;
+}
+//1 case solve
+// largest element ko end me dal dega
+for(int i =0; i<n-1;i++){
+    if(arr[i] > arr[i+1]){
+        swap(arr[i],arr[i+1]);
+    }
+}
+
+sortArray(arr,n-1);
+} 
+int main()
+{
+    int arr[] = {2,5,1,67,8};
+
+sortArray(arr,5);
+for(int i=0;i<5;i++){
+    cout<<arr[i]<<" ";
+}
+
+
+return 0;
+}
--- 4_Sorting/bubble_sort.cpp
+++ 4_Sorting/bubble_sort.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 void bubbleSort(vector<int> arr, int n)
 {   
-    for (int i = 1; i<n;i++){
         // for round n-1
+    for (int i = 1; i<n;i++){
+        bool swapped=false;
         for(int j =0; j<n-i; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped=true;
             }
+
         }
+            if(swapped == false){
+                break;
+            }
     }
 }
 
