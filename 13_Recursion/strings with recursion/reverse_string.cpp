8b42118c48bda34115b9efad87ce0b5969a4ed93
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-24 12:16:28 +0530

    reverse string using recursion

---
+++ 13_Recursion/strings with recursion/reverse_string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void reverse(string &s,int f ,int b){
+if(f>b) {
+    return;
+}
+swap(s[f],s[b]);
+
+reverse(s,f+1,b-1);
+
+}
+int main()
+{
+
+ int v[] = {2,4,5};   
+string s = "whatfuck";
+reverse(s,0,s.length()-1);
+cout<<s<<endl;
+
+
+
+
+
+
+
+
+return 0;
+}
--- 13_Recursion/sumOfArray.cpp
+++ 13_Recursion/sumOfArray.cpp
@@ -11,9 +11,19 @@ void isSum (int arr[],int size,int &ans){
       
       ans += arr[0]; 
      isSum(arr+1,size-1,ans);
-
-
-
+    
+}
+// without ref variable
+int Sum (int arr[],int size){
+    if(size == 0){
+        return 0;
+    }
+    if(size == 1){
+        return arr[0];
+    }
+      
+     int ans = arr[0] + Sum(arr+1,size-1); 
+     return ans;
     
 }
 int main()
@@ -21,7 +31,8 @@ int main()
 int arr[] = {1,2,4,5,6,8,2,4,6};
 int ans=0;
 isSum(arr,9,ans);
-cout<<ans;
+cout<<ans<<endl;
+cout<<Sum(arr,9);
 
 
 
