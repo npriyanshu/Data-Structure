e10482519268a0ff14efbd973226f9ee44943d17
Priyanshu Negi <npriyanshu63@gmail.com>
2024-03-04 20:45:44 +0530

    object oriented programming started

---
+++ 14_Oops/basic_intro.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+// creating a class
+
+class basic
+{
+private:
+ int arr[100];
+ int val;
+
+ public:
+ // getter setter
+ int  getVal() { return val; }
+ int * getArr() { return arr; }
+ void setVal(int v) { val  = v; }
+};
+
+
+int main()
+{
+
+// creating object of the class
+// static allocation
+basic obj;
+
+obj.setVal(50); // assigning value to 'val' member variable using dot operator
+
+cout<<"size :"<<sizeof(obj)<<endl;
+cout<<"value : "<<obj.getVal()<<endl;
+
+int * arr  = obj.getArr(); // getting address of 'arr'
+*arr=60; // changing the value
+cout<<*arr<<endl;
+
+
+// dynamic allocation
+
+basic *b = new basic;
+cout <<"size of b object :" <<sizeof(*b) << endl;
+
+// access data memebers in dynamic allocation
+
+// using dot and value at operator
+(*b).setVal(80);
+cout << "Value of 'val' through dot operator : " << (*b).getVal() << endl ;
+
+
+// without  using dot operator
+b->setVal(70);
+cout<<"Value : "<<b->getVal()<<endl;
+
+delete b; // deleting dynamically allocated memory
+
+
+
+
+
+return 0;
+}
