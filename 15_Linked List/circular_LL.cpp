ad9451e69bc54f6d8218486b39beb880c7adf4eb
Priyanshu Negi <npriyanshu63@gmail.com>
2024-03-10 00:16:08 +0530

    circular linked list comleted

---
+++ 15_Linked List/circular_LL.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = this;
+    }
+    // creating destructor to free the memory
+    ~Node()
+    {
+        int value = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        };
+        cout << " memory released for data " << value << endl;
+    }
+};
+
+void InsertNode(Node *&tail, int element, int d)
+{
+    // first case empty list
+    if (tail == NULL)
+    {
+        Node *temp = new Node(d);
+        tail = temp;
+        temp->next = temp;
+        return;
+    }
+    else
+    {
+        Node *curr = tail; // Start from the head (tail->next) of the list
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+       
+        Node *newNode = new Node(d);
+        newNode->next = curr->next;
+        curr->next = newNode;
+
+       
+    }
+}
+
+void print(Node *tail)
+{
+    Node *temp = tail;
+    // empty list
+ if(tail == NULL){
+    cout<<" Linked List is Empty!"<<endl;
+    return;
+ }
+
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+    cout<<endl;
+
+}
+
+// delete
+
+void deleteNode(Node* &tail,int value){
+
+    // if empty list
+    if(tail == NULL){
+        cout<<"linked list is empty"<<endl;
+        return;
+    }
+    else{
+  // assuminig that value is present in
+        // the list
+        Node* prev = tail;
+        Node* curr = tail->next;
+
+        // find element to delete
+
+        while(curr->data != value){
+            prev = curr;
+            curr = curr->next;
+        }
+
+        prev->next = curr->next;
+  // single node LL
+  if(prev == curr){
+    tail = NULL; 
+  }
+// <=2 node LL
+else if(curr==tail){
+    tail = prev;
+}
+
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+
+int main()
+{
+    Node * node = new Node(1);
+    // cout<<node->data<<endl;
+    Node* tail = node;
+
+    // InsertNode(tail,1,2);
+    // InsertNode(tail,2,3);
+    // InsertNode(tail,3,4);
+    // InsertNode(tail,4,5);
+    // InsertNode(tail,5,7);
+    print(tail);
+    deleteNode(tail,1);
+    // cout<<"tail "<<tail->data<<endl;
+    print(tail);
+
+
+return 0;
+}
